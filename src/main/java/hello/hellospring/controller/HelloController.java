ddc1139fde446790d694c241e204d34391d91236
ash2748 <max3535@naver.com>
2021-12-07 21:39:02 +0900

    웹개발기초 - 정적컨텐츠, mvc패턴 기초

--- src/main/java/hello/hellospring/controller/HelloController.java
+++ src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -16,5 +17,14 @@ public class HelloController {
         //스프링부트 템플릿엔진 기본 viewName매핑
         //resources:templates/ + {viewName} + .html 
         return "hello";
+    }
+
+    //hello-mvc URL로 요청이 온 경우
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model) {
+        //쿼리스트링으로 받은 name값을 모델에 담아 넘긴다.
+        model.addAttribute("name", name);
+        //viewResolver가 hello-template.html을 찾는다.
+        return "hello-template";
     }
 }
